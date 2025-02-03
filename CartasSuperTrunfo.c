88cc38d471a6694ccdbafc05c56a652f5446c5f3
Josivan1988 <josivandasilva88@gmail.com>
2025-02-03 15:14:02 +0000

    Criação do cadrastro das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,40 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    int codigo;
+    int populacao;
+    int turismo;
+    char nome[30];
+    float pib;
+    float area;
+
+    printf("Digite o codigo da cidade\n");
+    scanf("%d",&codigo);
+
+    printf("Digite o nome da cidade\n");
+    scanf("%s",&nome);
+    
+    printf("Digite a população\n");
+    scanf("%d",&populacao);
+
+    printf("Digite o quantidade de pontos turisticos \n");
+    scanf("%d", &turismo); 
+    
+    printf("Digite o Pib\n");
+    scanf("%f",&pib);
+     
+     printf("Digite a area\n");
+     scanf("%f",&area);
+
+    printf("Codigo:%d\n",codigo);
+    printf("Nome da cidade:%s\n",nome);
+    printf("População:%d\n",populacao);
+    printf("Pontos turisticos:%d\n",turismo);
+    printf("PIB:%f\n",pib);
+    printf("Área:%f\n",area);
+    
+
+
+
     return 0;
 }
